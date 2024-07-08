5b94cb0eae01adce2ac40d03c2bbb88bc538fd6a
vincevv017 <vince_v_v@yahoo.fr>
2024-07-08 19:35:35 +0200

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import requests
+import pandas
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -22,8 +23,14 @@ session = cnx.session()
 
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
