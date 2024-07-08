2bac79f1d12ad3d5ab95a4972afb4dfe3cd4fa42
vincevv017 <vince_v_v@yahoo.fr>
2024-07-08 18:05:21 +0200

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -36,6 +37,8 @@ if ingredients_list:
 
    for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
    #st.write(ingredients_string)
 
@@ -51,7 +54,5 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())    
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
